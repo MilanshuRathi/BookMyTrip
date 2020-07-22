506efff783b7a89c2451b61bcbdc38229ac54692
MilanshuRathi <milanshu2000@gmail.com>
2020-07-23 00:30:18 +0530

    updated the tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,39 +1,85 @@
 const Tour=require(`${__dirname}/../models/tourModel`);
-exports.getAllTours=(request,response)=>{
-    response.status(200).json({
-        status:'success',
-        data:{
-            message:'Data recieved'
-        }
-    });
+exports.getAllTours=async (request,response)=>{
+    try{
+        const tours=await Tour.find();
+        response.status(200).json({
+            status:'success',
+            results:tours.length,
+            data:{
+                tours
+            }
+        });
+    }
+    catch(err){
+        response.status(404).json({
+            status:'fail',
+            message:err
+        })
+    }
 };
-exports.createTour=(request,response)=>{
-    response.status(201).json({
-        status:'success',
-        data:{
-            message:'Data recieved'
-        }
-    });
+exports.createTour=async (request,response)=>{
+    try{
+        const newTour=await Tour.create(request.body);
+        response.status(201).json({
+            status:'success',
+            data:{
+                tour:newTour
+            }
+        });
+    }
+    catch(err){
+        response.status(400).json({
+            status:'fail',
+            message:err
+        });
+    }
 };
-exports.getTourById=(request,response)=>{
-    response.status(200).json({
-        status:'success',
-        data:{
-            message:'Data recieved'
-        }
-    });
+exports.getTourById=async(request,response)=>{
+    try{
+        const tour=await Tour.findById(request.params.id);
+        response.status(200).json({
+            status:'success',
+            data:{
+                tour
+            }
+        });
+    }    
+    catch(err){
+        response.status(404).json({
+            status:'fail',
+            message:err
+        });
+    }
 };
-exports.updateTourById=(request,response)=>{
-    response.status(200).json({
-        status:'success',
-        data:{
-            message:'Data recieved'
-        }
-    });
+exports.updateTourById= async(request,response)=>{
+    try{
+        const tour=await Tour.findByIdAndUpdate(request.params.id,request.body,{new:true,runValidators:true});        
+        response.status(200).json({
+            status:'success',
+            data:{
+                tour
+            }
+        });
+    }
+    catch(err){
+        response.status(404).json({
+            status:'fail',
+            message:err
+        });
+    }
 };
-exports.deleteTourById=(request,response)=>{
-    response.status(204).json({
-        status:'success',
-        data:null
-    });    
+exports.deleteTourById=async(request,response)=>{
+    try{
+        await Tour.findByIdAndDelete(request.params.id);
+        response.status(204).json({
+            status:'success',
+            data:null
+        });
+    }
+    catch(err){
+        response.status(400).json({
+            status:'fail',
+            message:err
+        })
+    }    
 };
