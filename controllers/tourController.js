8537b1f982ab526576b7c2bb1776f54e485366ab
MilanshuRathi <milanshu2000@gmail.com>
2020-07-24 22:57:16 +0530

    Fixed some bugs and added getTourStats

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,6 +6,41 @@ exports.top5ToursAliasFunc=(request,response,next)=>{
     request.query.fields='name,price,ratingsAverage,summary,difficulty';
     next();
 }
+exports.getTourStats=async (request,response)=>{
+    try{        
+        const stats=await Tour.aggregate([
+            {
+                $match:{ratingsAverage:{$gte:4.5}}
+            },
+            {
+                $group:{
+                    _id:{$toUpper:"$difficulty"},
+                    numTours:{$sum:1},
+                    numRatings:{$sum:"$ratingsQuantity"},
+                    avgRating:{$avg:"$ratingsAverage"},
+                    avgPrice:{$avg:"$price"},
+                    minPrice:{$min:"$price"},
+                    maxPrice:{$max:"$price"},
+                }
+            },
+            {
+                $sort:{avgRating:-1}
+            }
+        ]);
+        response.status(200).json({
+            status:'success',
+            data:{
+                stats
+            }
+        });
+    }
+    catch(err){
+        response.status(404).json({
+            status:'fail',
+            message:err
+        });
+    }
+};
 exports.getAllTours=async (request,response)=>{
     try{                    
         const tours=await new APIfeatures(Tour,request.query).filter().sort().fields().pagingation().query;                
