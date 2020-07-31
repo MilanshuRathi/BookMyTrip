daaa24debf7c17fd77453bfd59c14f7bc7c08b88
MilanshuRathi <milanshu2000@gmail.com>
2020-07-31 23:48:09 +0530

    added updateUser and deleteUser routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,15 @@ const express=require('express');
 const router=express.Router();
 const userController=require(`${__dirname}/../controllers/userController`);
 const authController=require(`${__dirname}/../controllers/authController`);
-//different route for signUp ...doesn't satisfy REST arch 100%
+//routes according to events 
 router.post('/signUp',authController.signUp);
 router.post('/login',authController.login);
 router.post('/forgotPassword',authController.forgotPassword);
 router.patch('/resetPassword/:token',authController.resetPassword);
 router.patch('/updatePassword',authController.protect,authController.updatePassword);
+router.patch('/updateMe',authController.protect,userController.updateMe);
+router.delete('/deleteMe',authController.protect,userController.deleteMe);
+//general route
 router.route('/')
 .get(userController.getAllUsers);
 
