e5de313781edc28205ac9451c3fdd06604e58855
MilanshuRathi <milanshu2000@gmail.com>
2020-07-31 23:46:48 +0530

    added updateUser and deleteUser functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
 const mongoose=require('mongoose');
+const { compareSync } = require('bcryptjs');
+const { response } = require('express');
 const User=require(`${__dirname}/../models/userModel`);
 const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
 const AppError=require(`${__dirname}/../utils/AppError`);
 const APIfeatures = require(`${__dirname}/../utils/APIfeatures`);
+
+//Util Methods
+const filterObj=(obj,...arr)=>{
+ const mp={},newObj={};
+    for(let i=0;i<arr.length;i++)
+        mp[arr[i]]=true;    
+    for(const el in obj){
+        if(mp[el])
+            newObj[el]=obj[el];
+    }
+    return newObj;
+}
+
+
+//Exporting methods
 exports.getAllUsers=catchAsyncError(async (request,response,next)=>{
     const users=await User.find();
     response.status(200).json({
@@ -21,6 +38,22 @@ exports.getUserById=(request,response)=>{
         }
     });
 };
+exports.updateMe=catchAsyncError(async (request,response,next)=>{
+    if(request.body.password||request.body.passwordConfirm)
+        return next(new AppError('This route is not for password updates,Please use /updatePassword',400));
+    //Filer user data ..to remove data other than required to terminate any malicious code 
+    const filteredObj=filterObj(request.body,'name','email');
+    //Get the updated user with given details    
+    console.log(filteredObj);     
+    const updatedUser=await User.findByIdAndUpdate(request.user._id,filteredObj,{new:true,runValidators:true});
+    //Send the updatedUser response
+    response.status(200).json({
+        status:'success',
+        data:{
+            updatedUser
+        }
+    });
+});
 exports.updateUserById=(request,response)=>{
     response.status(500).json({
         status:'error',
@@ -29,6 +62,13 @@ exports.updateUserById=(request,response)=>{
         }
     });
 };
+exports.deleteMe=catchAsyncError(async(request,response,next)=>{
+    await User.findByIdAndUpdate(request.user._id,{active:false});
+    response.status(204).json({
+        status:'success',
+        data:null
+    });    
+});
 exports.deleteUserById=(request,response)=>{
     response.status(500).json({
         status:'error',
