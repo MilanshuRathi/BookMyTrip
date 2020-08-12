bc05ed9c7cd881344962bfe14601494fec62312d
MilanshuRathi <milanshu2000@gmail.com>
2020-08-13 00:50:37 +0530

    handlers for get/post of reviews by specific tours

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,11 @@
 const express=require('express');
-const router=express.Router();
+const router=express.Router({mergeParams:true});//here merge params merges all the parameters like tourId,id of the parent routes too in this route i.e we have access to all params 
 const routeController=require(`${__dirname}/../controllers/reviewController`);
 const authController=require(`${__dirname}/../controllers/authController`);
-router.route('/').get(routeController.getAllReviews).post(authController.protect,authController.restrictTo('user'),routeController.createReview);
+//Route handlers
+//POST /:tourId/reviews , /reviews
+//GET /:tourId/reviews , /reviews
+router.route('/')
+.get(routeController.getAllReviews)
+.post(authController.protect,authController.restrictTo('user'),routeController.createReview);
 module.exports=router;
