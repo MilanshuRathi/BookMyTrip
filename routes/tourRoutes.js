dcaecdb4421a4e1ca46c805b348eced618431ca0
MilanshuRathi <milanshu2000@gmail.com>
2020-08-13 00:49:46 +0530

    nested reviewRouter in tourRouter

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,12 +2,14 @@ const express=require('express');
 const router=express.Router();
 const tourController=require(`${__dirname}/../controllers/tourController`);
 const authController=require(`${__dirname}/../controllers/authController`);
+const reviewRouter=require(`${__dirname}/../routes/reviewRoutes`);
 router.route('/top-5-tours').get(tourController.top5ToursAliasFunc,tourController.getAllTours);
 router.route('/tour-stats/:year').get(tourController.getTourStats);
 router.route('/')
 .get(authController.protect,tourController.getAllTours)
 .post(tourController.createTour);
 
+router.use('/:tourId/reviews',reviewRouter);
 //Handles get,patch,delete request specific to id
 router.route('/:id')
 .get(tourController.getTourById)
