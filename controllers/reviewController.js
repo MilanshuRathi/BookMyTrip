90684b3082fd90400e32fa196ac12ec01a803e04
MilanshuRathi <milanshu2000@gmail.com>
2020-08-13 00:53:13 +0530

    some minor changes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,11 +1,12 @@
 const Review=require(`${__dirname}/../models/reviewModel`);
 const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
-const AppError=require(`${__dirname}/../utils/AppError`);
-// const catchAsyncError=require(`${__dirname}/../utils/API`);
+// const AppError=require(`${__dirname}/../utils/AppError`);
 
 //Methods
 exports.getAllReviews=catchAsyncError(async (request,response,next)=>{
-    const reviews=await Review.find();//add request.query    
+    let filter={};
+    if(request.params.tourId) filter={tour:request.params.tourId};
+    const reviews=await Review.find(filter);//gets reviews of a specific tour when tourId is mentioned
     response.status(200).json({
         status:'success',
         results:reviews.length,
@@ -15,6 +16,8 @@ exports.getAllReviews=catchAsyncError(async (request,response,next)=>{
     });
 });
 exports.createReview=catchAsyncError(async (request,response,next)=>{
+    if(!request.body.tour) request.body.tour=request.params.tourId;
+    if(!request.body.user) request.body.user=request.user.id;
     const newReview=await Review.create({
         review:request.body.review,
         rating:request.body.rating,
