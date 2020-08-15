fff9122ab070f50152d0e3347e87cc138e4d3a6b
MilanshuRathi <milanshu2000@gmail.com>
2020-08-16 03:29:38 +0530

    added geospacial data routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,8 @@ const authController=require(`${__dirname}/../controllers/authController`);
 const reviewRouter=require(`${__dirname}/../routes/reviewRoutes`);
 router.route('/top-5-tours').get(tourController.top5ToursAliasFunc,tourController.getAllTours);
 router.route('/tour-stats/:year').get(authController.protect,authController.restrictTo('admin','lead-guide','guide'),tourController.getTourStats);
+router.route('/tours-within/:distance/center/:latlong/unit/:unit').get(tourController.getTourWithin);
+router.route('/tours-within/:latlong/unit/:unit').get(tourController.getDistances);
 router.route('/')
 .get(tourController.getAllTours)
 .post(authController.protect,authController.restrictTo('admin','lead-guide'),tourController.createTour);
