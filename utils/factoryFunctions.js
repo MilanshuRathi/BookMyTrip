d3755e7ea24f99ccc5792d6f79d71f0c48071e44
MilanshuRathi <milanshu2000@gmail.com>
2020-08-15 15:20:50 +0530

    added getOne and getALl in factoryFunctions

--- utils/factoryFunctions.js
+++ utils/factoryFunctions.js
@@ -1,5 +1,6 @@
 const catchAsyncError=require(`${__dirname}/catchAsyncError`);
 const AppError=require(`${__dirname}/AppError`);
+const APIfeatures=require(`${__dirname}/APIfeatures`);
 exports.deleteOne=Model=>catchAsyncError(async (request,response,next)=>{
     const doc=await Model.findByIdAndDelete(request.params.id);
     if(!doc)
@@ -29,14 +30,29 @@ exports.createOne=Model=>catchAsyncError(async (request, response, next) => {
         }
     });    
 });
-exports.getOne=Model=>catchAsyncError(async (request, response, next) => {    
-    const doc = await Tour.findById(request.params.id).populate('reviews');
-    if(!tour)
+exports.getOne=(Model,populateOptions)=>catchAsyncError(async (request, response, next) => {    
+    let query= Model.findById(request.params.id);
+    if(populateOptions) 
+        query=query.populate(populateOptions);
+    const doc = await query;
+    if(!doc)
         return next(new AppError('No tour found with that ID',404));
     response.status(200).json({
         status: 'success',
         data: {
-            tour
+            "document":doc
         }
     });            
+});
+exports.getAll=Model=>catchAsyncError(async (request, response, next) => {                     
+    let filter=null;
+    if(request.params.tourId) filter={tour:request.params.tourId};
+    const doc = await new APIfeatures(filter?Model.find(filter):Model, request.query).filter().sort().fields().pagination().query;    
+    response.status(200).json({
+        status: 'success',
+        results: doc.length,
+        data: {
+            "document":doc
+        }
+    });    
 });
