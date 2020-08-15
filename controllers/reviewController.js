db6b074923e9a8f1df4ca3a7d0d4862b8977709f
MilanshuRathi <milanshu2000@gmail.com>
2020-08-15 15:22:37 +0530

    added few routeHandlers

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,23 +2,13 @@ const Review=require(`${__dirname}/../models/reviewModel`);
 const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
 const factory=require(`${__dirname}/../utils/factoryFunctions`);
 //Methods
-exports.getAllReviews=catchAsyncError(async (request,response,next)=>{
-    let filter={};
-    if(request.params.tourId) filter={tour:request.params.tourId};
-    const reviews=await Review.find(filter);//gets reviews of a specific tour when tourId is mentioned
-    response.status(200).json({
-        status:'success',
-        results:reviews.length,
-        data:{
-            reviews
-        }
-    });
-});
 exports.setTourandUser=(request,response,next)=>{
     if(!request.body.tour) request.body.tour=request.params.tourId;
     if(!request.body.user) request.body.user=request.user.id;
     next();
 }
+exports.getAllReviews=factory.getAll(Review);
+exports.getReviewById=factory.getOne(Review);
 exports.createReview=factory.createOne(Review);
 exports.updateReview=factory.updateOne(Review);
 exports.deleteReview=factory.deleteOne(Review);
