156d76b8abcf3af679315d0b0becee776690df37
MilanshuRathi <milanshu2000@gmail.com>
2020-08-17 02:31:04 +0530

    added a login verifier

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
 const viewController=require(`${__dirname}/../controllers/viewController`);
+const authController=require(`${__dirname}/../controllers/authController`);
+router.use(authController.isLoggedin);//to check if user is logged in or not and it will run for each and every request
 router.get('/',viewController.getOverview);
-router.get('/tour',viewController.getTour);
+router.get('/tour/:tourSlug',viewController.getTour);
+router.get('/login',viewController.getLoginPage);
 module.exports=router;
