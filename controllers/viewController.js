a8bf190f11a135e671235bf3a7a437e25f5448a1
MilanshuRathi <milanshu2000@gmail.com>
2020-08-18 00:34:36 +0530

    added login and signup func

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,7 @@
-const { request, response } = require("express");
-
 const Tour=require(`${__dirname}/../models/tourModel`);
-const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
-exports.getOverview=catchAsyncError(async(request,response)=>{    
+const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`)
+const AppError=require(`${__dirname}/../utils/AppError`);
+exports.getOverview=catchAsyncError(async(request,response,next)=>{    
     //1) get tours data from Tour model
     const tours=await Tour.find();//await Tour.find();
     //2)Build template
@@ -12,12 +11,15 @@ exports.getOverview=catchAsyncError(async(request,response)=>{
         tours
     });
 });
-exports.getTour=catchAsyncError(async(request,response)=>{       
+exports.getTour=catchAsyncError(async(request,response,next)=>{       
     //1) get tour data from Tour model
     const tour=await Tour.findOne({slug:request.params.tourSlug}).populate({
         path:'reviews',
         fields:'review rating user'
-    });//find that tour from slug    
+    });
+    if(!tour)
+        return next(new AppError('There is no tour with that name',404));
+    //find that tour from slug    
     //2)Build template
     // response.status(200).json({
     //     tour
@@ -29,5 +31,17 @@ exports.getTour=catchAsyncError(async(request,response)=>{
     });
 });
 exports.getLoginPage=(request,response,next)=>{
-    response.status(200).render('login',{title:'Login'});
+    if(!response.locals.user)
+        response.status(200).render('login',{title:'Login'});
+    else    
+        response.redirect('/');
+};
+exports.getSignUpPage=(request,response,next)=>{
+    if(!response.locals.user)        
+        response.status(200).render('signup',{title:'Create your Account'});
+    else    
+        response.redirect('/');
+};
+exports.getAccount=(request,response)=>{
+    response.status(200).render('account',{title:'My account'});
 };
