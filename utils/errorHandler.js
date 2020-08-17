cafd9396ed5ed2c4c3f4dda3b54c3ae55794aadc
MilanshuRathi <milanshu2000@gmail.com>
2020-08-18 00:35:29 +0530

    modified error handler funcs

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
 const AppError = require(`${__dirname}/AppError`);
-const sendDevErr = (error, response) => {
-    // console.log(error instanceof mongoose.Error.MongoError);
-    response.status(error.statusCode).json({
-        status: error.status,
-        error,
-        message: error.message,
-        stack: error.stack
-    });
+const sendDevErr = (error,request, response) => {
+    //API ERRPR
+    if(request.originalUrl.startsWith('/api'))
+        response.status(error.statusCode).json({
+            status: error.status,
+            error,
+            message: error.message,
+            stack: error.stack
+        });
+    //WEBSITE ERROR
+    else
+        response.status(error.statusCode).render('error',{
+            title:'Something went wrong!',
+            message:error.message
+        });
 };
 const handleCastErrorDB = (error) => {
     message = `Invalid ${error.path}: ${error.value}`;
@@ -24,25 +31,44 @@ const handleMongoErrorDB = (error) => {
     const message = `Duplicate field value: ${value}. Please use another value!`;
     return new AppError(message, 400);
 }
-const sendProdErr = (error, response) => {
-    if (error.isOperational)
-        response.status(error.statusCode).json({
-            status: error.status,
-            message: error.message
-        });
-    else {
-        console.log('Programming error occured');
-        response.status(500).json({
-            status: 'error',
-            message: 'Something went wrong'
-        });
+const sendProdErr = (error,request, response) => {
+    //A) API Error
+    if(request.originalUrl.startsWith('/api')){
+        if (error.isOperational)
+            response.status(error.statusCode).json({
+                status: error.status,
+                message: error.message
+            });
+        else {
+            console.log('Programming error occured');
+            response.status(500).json({
+                status: 'error',
+                message: 'Something went wrong'
+            });
+        }
     }
+    //WEBSITE ERROR
+    else{
+        if (error.isOperational)
+            response.status(error.statusCode).render('error',{
+                title:'Something went wrong!',
+                message:error.message
+            });
+        else {
+            console.log('Programming error occured');
+            response.status(error.statusCode).render('error',{
+                title:'Something went wrong!',
+                message:'Please try again later'
+            });
+        }        
+    }
+    
 };
 module.exports = (error, request, response, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
     if (process.env.NODE_ENV === 'development')
-        sendDevErr(error, response);
+        sendDevErr(error,request, response);
     else {
         // let errorTemp={...error};        
         if (error instanceof mongoose.Error.CastError) error = handleCastErrorDB(error);
@@ -50,7 +76,7 @@ module.exports = (error, request, response, next) => {
         else if (error.code === 11000) error = handleMongoErrorDB(error);
         else if (error.name === 'JsonWebTokenError') error = new AppError('Invalid token. Please log in again!', 401);
         else if (error.name === 'TokenExpiredError') error = new AppError('Your token has expired! Please log in again.', 401);
-        sendProdErr(error, response);
+        sendProdErr(error,request, response);
     }
     next();
 }
