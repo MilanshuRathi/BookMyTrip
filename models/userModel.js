f0de77f2301b04aacad1741e4e36d574bbb837d6
MilanshuRathi <milanshu2000@gmail.com>
2020-08-19 15:38:48 +0530

    added a small detail

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,10 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please provide your email"],
         validate: [validator.isEmail, "Please provide a valid email"],
     },
-    photo: String,
+    photo: {
+        type:String,
+        default:'default.jpg'
+    },
     role:{
         type:String,
         enum:['user','admin','guide','lead-guide'],
