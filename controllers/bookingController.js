04c36fefccc9d6442169d3892975e01779edf531
MilanshuRathi <milanshu2000@gmail.com>
2020-08-22 20:39:31 +0530

    removed console log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -36,7 +36,6 @@ const createBookingCheckout=async session=>{
     const tour=session.client_reference_id;
     const user=(await User.findOne({email:session.customer_email})).id;
     const price=(session.display_items[0].amount)/100;
-    console.log(tour,user,price);
     await Booking.create({tour,user,price});
 };
 exports.webhookCheckout=(request,response,next)=>{
