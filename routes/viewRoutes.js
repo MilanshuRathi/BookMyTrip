04f9259c6921914ed90ca899b313a2bbc33d373a
MilanshuRathi <milanshu2000@gmail.com>
2020-08-22 21:02:21 +0530

    added alert controller to routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const viewController=require(`${__dirname}/../controllers/viewController`);
 const bookingController=require(`${__dirname}/../controllers/bookingController`);
 const authController=require(`${__dirname}/../controllers/authController`);
 // authController.isLoggedin  =>  to check if user is logged in or not and it will run for each and every request
+router.use(viewController.alerts);
 router.get('/me',authController.protect,viewController.getAccount);
 router.get('/my-bookings',authController.protect,viewController.getBookings);
 router.get('/my-reviews',authController.protect,viewController.getReviews);
