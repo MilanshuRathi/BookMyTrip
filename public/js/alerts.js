75368fe5163023b01331a72369584b56be4e25a3
MilanshuRathi <milanshu2000@gmail.com>
2020-08-22 21:02:56 +0530

    added variable time to alerts

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,8 +1,8 @@
-export const showAlert=(type,message)=>{
+export const showAlert=(type,message,time=3)=>{
     hideAlert();    
     const htmlString=`<div class='alert alert--${type}'>${message}</div>`;
     document.querySelector('body').insertAdjacentHTML('afterbegin',htmlString);
-    window.setTimeout(()=>{hideAlert();},3000);
+    window.setTimeout(()=>{hideAlert();},time*1000);
 }
 const hideAlert=()=>{
     const el=document.querySelector('.alert');
--- public/js/index.js
+++ public/js/index.js
@@ -122,3 +122,6 @@ if(reviewForm){
         submitReview(tourId,reviewVal,rating);        
     }); 
 }
+const alertMessage=document.querySelector('body').dataset.alert;
+if(alertMessage)
+    showAlert('success',alertMessage,10);
