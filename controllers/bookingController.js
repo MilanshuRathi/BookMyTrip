c5d9ecf704b9d1c24baaa5e6e06e519e88e784d7
Milanshu Rathi <milanshu2000@gmail.com>
2021-05-29 13:39:22 +0530

    fix: fixed bug in bookingController

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -35,7 +35,7 @@ exports.getCheckoutSession=catchAsyncError(async (request,response,next)=>{
 const createBookingCheckout=async session=>{
     const tour=session.client_reference_id;
     const user=(await User.findOne({email:session.customer_email})).id;
-    const price=(session.display_items[0].amount)/100;
+    const price=(session.amount_total)/100;
     await Booking.create({tour,user,price});
 };
 exports.webhookCheckout=(request,response,next)=>{
@@ -47,7 +47,6 @@ exports.webhookCheckout=(request,response,next)=>{
             signature,
             process.env.STRIPE_WEBHOOK_KEY
         );
-        console.log(event);
         if(event.type==='checkout.session.completed')
         createBookingCheckout(event.data.object);
         response.status(200).json({
